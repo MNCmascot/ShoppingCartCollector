ba3c01999771b7cac4fb1fb53aa83df58d3eae82
mncmascot <aidan.gowsell@gmail.com>
2021-01-18 12:10:22 -0500

    Added trailing carts behind the player (no collisions)

--- app/src/main/java/com/example/shoppingcartcollector/gameobject/Player.java
+++ app/src/main/java/com/example/shoppingcartcollector/gameobject/Player.java
@@ -28,6 +28,12 @@ public class Player extends GameObject{
     private Paint paint;
     private boolean dead;
 
+    //data for drawing trailing carts
+    private float cartWidth;
+    private float cartHeight;
+    private double movementAngle;
+    private Paint cartPaint;
+
     public Player(Context context, Joystick joystick, double positionX, double positionY, double radius) {
         super(positionX, positionY);
         this.joystick = joystick;
@@ -35,10 +41,20 @@ public class Player extends GameObject{
         this.cartsCollected = 0;
         this.dead = false;
 
+        //set up extra info for drawing trailing carts
+        this.cartWidth = 60; //manually set
+        this.cartHeight = 40; //manually set
+        this.movementAngle = 0; //initialize as 0
+
         //set up player colour
         paint = new Paint();
         int colour = ContextCompat.getColor(context, R.color.player);
         paint.setColor(colour);
+
+        //set up Cart colour
+        cartPaint = new Paint();
+        int colour2 = ContextCompat.getColor(context, R.color.cart);
+        cartPaint.setColor(colour2);
 
     }
 
@@ -49,7 +65,19 @@ public class Player extends GameObject{
     }
 
     public void draw(Canvas canvas) {
+        //draw player
         canvas.drawCircle((float)positionX, (float)positionY, (float)radius, paint);
+
+        //draw trailing carts
+        canvas.save(); //save canvas state before rotating
+
+        //rotate canvas in movement direction (to draw carts trailing behind
+        canvas.rotate((float)movementAngle, (float)positionX, (float)positionY);
+        for (int i = 1; i <= cartsCollected; i++) {
+            canvas.drawRect((float) (positionX - 60*i - radius), (float) (positionY - 20),
+                    (float) (positionX - 60*(i-1) - radius), (float) (positionY + 20), cartPaint);
+        }
+        canvas.restore(); //restore rotation
     }
 
     public void update() {
@@ -57,6 +85,12 @@ public class Player extends GameObject{
         velocityX = joystick.getActuatorX()*MAX_SPEED;
         velocityY = joystick.getActuatorY()*MAX_SPEED;
 
+        //get direction that player is moving in and turn to degrees
+        double tempAngle = Math.toDegrees(Math.atan2(joystick.getActuatorY(), joystick.getActuatorX()));
+        //only update if player is moving so it doesn't snap back behind them
+        if (tempAngle != 0) {
+            movementAngle = Math.toDegrees(Math.atan2(joystick.getActuatorY(), joystick.getActuatorX()));
+        }
         //update position
         positionX += velocityX;
         positionY += velocityY;
